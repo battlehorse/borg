8f4a1cb404be83dcab923a8f488703d542175a4a
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-05-05 23:19:21 +0000

    Rolling back changes to borg_development settings
    git-svn-id: http://svn.assembla.com/svn/borg@24 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails2.3.2/config/environments/borg_development.rb
+++ branches/rails2.3.2/config/environments/borg_development.rb
@@ -1,47 +1,47 @@
 # Borg variables here
 BorgConfig = {
   # Site title
-  :title => "Borg<span class='title'>!</span>",
+  :title => "Your Site",
   
   # Site tagline
-  :tagline =>  "The Battlehorse ORGanizer",
+  :tagline =>  "This is really your site",
   
   # Directory where all the pages are stored
-  :data_dir => "/Users/battlehorse/Documents/Devel/ruby/rails/borg-data/data2/",
+  :data_dir => "/path/to/data/dir/",
   
   # DIrectory where all the comments are stored
-  :comment_dir => "/Users/battlehorse/Documents/Devel/ruby/rails/borg-data/comment2/",
-
+  :comment_dir => "/path/to/comment/dir/",
+  
   # CSS Template: 'InternetSharing', 'CoolWater'
-  :css_template => "InternetSharing",
+  :css_template => "InternetSharing",  
   
   # DEFIMG url
-  :default_img_url => "http://www.battlehorse.net/defimg/",
+  :default_img_url => "http://url.for.default.images",
   
   # Url for the contact link that appears on top of every page
-  :contact_url => "http://localhost:3000/page/people/me/index.html",
+  :contact_url => "http://url.for.the.contact.page",
   
   # List of editors. All the OpenID urls listed here will be able to edit pages.
   # Remember to put the whole URL, including the scheme part.
   # 
   # WARNING: Leaving this list empty will create an open website! Everybody will be able to edit.
-  :editors => [ 'http://battlehorse.pip.verisignlabs.com/' ],
+  :editors => [ 'http://editor.openid' ],
   
   # Meta information about the site, that will be used to populate
   # HTML META tags on top of each page.
   :meta =>  {
-    :author => 'Riccardo Govoni - battlehorse@gmail.com',
+    :author => 'Your Name - yourmail@domain.com',
     :robots => 'index,follow',
-    :keywords => 'information technology, user interface, web design, java, python, web standard, javascript',
-    :description => 'Information architecture, Web Design, Web Standards.'
-  },
+    :keywords => 'your, keywords',
+    :description => 'your site description'
+  },  
   
   # Domain to be searched from the search box (that uses google search)
-  :search_domain => "battlehorse.net",
+  :search_domain => "yourdomain.com",
   
   # Google analytics account. Comment this line if you don't have any
-  :google_analytics => "UA-328792-1",
+  :google_analytics => "UA-123456-1",
   
   # Googla chatback. Paste here the content as generated from http://www.google.com/talk/service/badge/New, if you want to use it.
-  :google_chatback => '<iframe src="http://www.google.com/talk/service/badge/Show?tk=z01q6amlqupsnbfd19tbnltt8g9cmv7rf2p4f3hec8vetl94b380jjo59a49n9rcss57af67g92gqgfd5nlb8ioq34dj0576avm0m6dr3l7k4vnqcgukudi2o1uotnfhegu5v3u1tmjvitvcsthpjeoacrr3u7gs1qjbrou9l&amp;w=159&amp;h=36" allowtransparency="true" frameborder="0" height="36" width="159"></iframe>'
+  :google_chatback => ''
 }
