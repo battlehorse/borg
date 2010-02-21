55e927636b8641e116b407c7773c9a132b9d6b64
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-02-21 12:34:54 +0000

    Updated version numbers for rails 2.3.5
    git-svn-id: http://svn.assembla.com/svn/borg@28 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails2.3.5/config/environment.rb
+++ branches/rails2.3.5/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,9 +19,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'ruby-openid', :lib => 'openid', :version => '2.1.6'
-  config.gem "rubytree", :lib => 'tree', :version => "0.5.2"
-  config.gem "RedCloth", :version => "4.1.9"
+  config.gem 'ruby-openid', :lib => 'openid', :version => '2.1.7'
+  config.gem "rubytree", :lib => 'tree', :version => "0.6.2"
+  config.gem "RedCloth", :version => "4.2.2"
   config.gem "chronic", :version => '0.2.3'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
