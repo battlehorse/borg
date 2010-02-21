a0162ae31dc7557bb0857fd2b918c25bd61027d0
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-02-21 12:06:34 +0000

    Opening rails2.3.5 branch
    git-svn-id: http://svn.assembla.com/svn/borg@26 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ branches/rails2.3.5/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_rails2.3.5_session',
+  :secret      => '3e8ec6cfd13b0491c9a179c4be664e9a770bf23ca3bad221a7a35b4d5d257a05886fe915684a5029d6ed27775384da51b7d2153c3485bf8fd3ecbfb6b437e46d'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
