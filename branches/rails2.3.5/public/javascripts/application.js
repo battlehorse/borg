4f25541a0672369c5fc3b6e0fb25269835aa5135
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-05-17 22:41:46 +0000

    Refactoring javascript code out of the mobile layout page.
    git-svn-id: http://svn.assembla.com/svn/borg@53 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails2.3.5/public/javascripts/application.js
+++ branches/rails2.3.5/public/javascripts/application.js
@@ -11,3 +11,24 @@ borg.translate = function(select, url) {
   var language = select[select.selectedIndex].value;
   document.location.href = "http://translate.google.com/translate?u=" + encodeURIComponent(url) + "&sl=en&tl=" + language;
 };
+
+borg.updateOrientation = function() {
+  switch(window.orientation) {
+    case 0:  // Portrait
+      $('global-wrap').removeClassName('landscape');
+      $('global-wrap').addClassName('portrait'); 
+      break;
+    case -90:  // Landscape
+      $('global-wrap').addClassName('landscape');
+      $('global-wrap').removeClassName('portrait');
+      break;
+    case 90:
+      $('global-wrap').addClassName('landscape');
+      $('global-wrap').removeClassName('portrait');      
+      break;
+    case 180:
+      $('global-wrap').removeClassName('landscape');
+      $('global-wrap').addClassName('portrait');
+      break;
+  }
+};
