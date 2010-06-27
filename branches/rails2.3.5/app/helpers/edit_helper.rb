6e8f2bbeaeb12c6f10cad62afeb5515a65192532
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-06-27 23:48:03 +0000

    Adding Wave create boilerplate.
    git-svn-id: http://svn.assembla.com/svn/borg@67 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails2.3.5/app/helpers/edit_helper.rb
+++ branches/rails2.3.5/app/helpers/edit_helper.rb
@@ -11,17 +11,44 @@ module EditHelper
     return raw_content.blank? ? nil : raw_content
   end
   
-  def boilerplate
+  def boilerplate(kind)
+    if kind == 'wave'
+      wave_boilerplate
+    else
+      # assume kind == 'page'
+      page_boilerplate
+    end
+  end
+  
+  def wave_boilerplate
     # Ugly, but needed to avoid whitespaces...
     return <<-EOF
-# author : #{session[:user_id] ? session[:user_id].to_author_hash['author'] : 'Name Surname'}
-# author_mail : #{session[:user_id] ? session[:user_id].to_author_hash['author_mail'] : 'address at foo dot com'}
-# tags : one, two, three
-# date : #{Time.now.strftime('%b %d %Y')}
+#{headers_boilerplate(true)}
+
+h1. Your Title here
+
+!(articleimg float-right)#google-wave-logo.png#! Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+EOF
+  end
+  
+  def page_boilerplate
+    # Ugly, but needed to avoid whitespaces...
+    return <<-EOF
+#{headers_boilerplate(false)}
 
 h1. Your Title here
 
 !(articleimg float-right)http://rubyonrails.org/images/rails.png! Lorem ipsum dolor sit amet, consectetur adipisicing elit...
-    EOF
+EOF
+  end
+  
+  def headers_boilerplate(is_wave)
+    headers = ""
+    headers << "# author : #{session[:user_id] ? session[:user_id].to_author_hash['author'] : 'Name Surname'}\n"
+    headers << "# author_mail : #{session[:user_id] ? session[:user_id].to_author_hash['author_mail'] : 'address at foo dot com'}\n"
+    headers << "# tags : #{is_wave ? 'wave, ' : ''}one, two, three\n"
+    headers << "# date : #{Time.now.strftime('%b %d %Y')}\n"
+    headers << "# wave : googlewave.com!wave_id\n"
+    return headers
   end
 end
