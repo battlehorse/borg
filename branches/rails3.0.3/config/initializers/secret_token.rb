ce234c93f4d7d92541d454e3cdca1c75de0ea64d
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-31 10:45:27 +0000

    Opening rails 3.0.3 branch
    git-svn-id: http://svn.assembla.com/svn/borg@75 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ branches/rails3.0.3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails303::Application.config.secret_token = '88ed2bc615392b30926807a99bf5f6565145414a0c45c32ec9b6047f1ebd67f6414b6598815b008c46a7be5a6d791d162473ccc88d0d43794bedb4f62ddd4c7a'
