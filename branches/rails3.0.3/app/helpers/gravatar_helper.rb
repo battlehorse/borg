5ad728f9de1159058a0e5f2b0eb24c0a34836814
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-01-02 21:18:13 +0000

    Marking gravatar images as html safe.
    git-svn-id: http://svn.assembla.com/svn/borg@91 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails3.0.3/app/helpers/gravatar_helper.rb
+++ branches/rails3.0.3/app/helpers/gravatar_helper.rb
@@ -6,7 +6,7 @@ module GravatarHelper
     digest = Digest::MD5.new.update(email.gsub(/ at /,"@").gsub(/ dot /,".").downcase)
     grav_url = "http://www.gravatar.com/avatar/#{digest}?s=60&r=g&d=wavatar"
   
-    "<img src=\"#{grav_url}\" alt=\"#{email}\" class=\"articleimg float-right\" />"
+    "<img src=\"#{grav_url}\" alt=\"#{email}\" class=\"articleimg float-right\" />".html_safe
   end
   
 end
