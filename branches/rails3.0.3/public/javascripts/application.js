1f62d7c1bbbfddd448a5b1f74e20ea47cfaa94a7
battlehorse <battlehorse@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-01-03 18:03:12 +0000

    Replaced handmade google translate support with Google Translate WebElement.
    git-svn-id: http://svn.assembla.com/svn/borg@106 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/rails3.0.3/public/javascripts/application.js
+++ branches/rails3.0.3/public/javascripts/application.js
@@ -1,17 +1,6 @@
 // borg namespace
 var borg = {};
 
-borg.show_translate_box = function() {
-  $('translateLink').hide();
-  $('translateBox').show();
-  $('translateSelect').selectedIndex = 0;
-};
-
-borg.translate = function(select, url) {
-  var language = select[select.selectedIndex].value;
-  document.location.href = "http://translate.google.com/translate?u=" + encodeURIComponent(url) + "&sl=en&tl=" + language;
-};
-
 borg.updateOrientation = function() {
   switch(window.orientation) {
     case 0:  // Portrait
